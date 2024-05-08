2188d8c784d57d3c7bdf43f1de1a7b1be7afebb5
DeadMartyr <Angelo.S.Arriaga@outlook.com>
2024-05-07 20:42:49 -0700

    Program outputs Possible Board states that fufill the 4-Queens Requirement

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
 
     private static Random rand = new Random();
+    private static int[] posQueens;
 
     public static void main(String[] args) {
         //This is unneeded as the Random object is seeded in its constructor
@@ -10,6 +12,12 @@ public class Main {
         //rand.setSeed(System.currentTimeMillis());
 
         System.out.println("Random Number: " + rand.nextInt(5));
+
+        int NumOfQueens = 4;
+
+        init(NumOfQueens);
+        nQueens(0,NumOfQueens);
+
     }
 
 //    private int estimate(){
@@ -33,4 +41,70 @@ public class Main {
 //        }
 //        return numnodes;
 //    }
+
+    /**
+     * Initialize global variables
+     *
+     * @param n - max dimension of board
+     */
+    private static void init(int n){
+        posQueens = new int[n];
+    }
+
+    /**
+     * nQueens recursively call
+     *
+     * @param k - current loop
+     * @param n - max dimension of board
+     */
+    private static void nQueens(int k, int n){
+        for(int i=0; i<n; i++){
+            System.out.println("k: " + k + " i: " + i);
+            if(isPromising(k,i)){
+                posQueens[k]=i;
+                if(k==n-1){
+                    printPositions();
+                    System.out.println();
+                    return;
+                }
+                else{
+                    nQueens(k+1,n);
+                }
+            }
+        }
+    }
+
+    /**
+     * Checks if a given position at index "i" of the k-th row is worth continuing
+     * <p>
+     * NO OTHER QUEENS ON SAME COLUMN:
+     * If on row "k" a Queen is on column "i", and there exists another row "j" in which
+     * that row's queen is also on column "i". Then this permutation is NOT promising.
+     * <p>
+     * NO OTHER QUEENS ON DIAGONALS:
+     * If the column index of a Queen on any row "j" is the same distance from index "i"
+     * as the difference between the indexes "j" and "k". Then this permutation is NOT promising.
+     * <br>(i.e. if the queen on row 4 is on column 6, then on row 3, there cannot be a queen on
+     * column 5 or 7. All other positions irrelevant.)
+     *
+     * @param k - The row being filled
+     * @param i - A possible index to fill row "k"
+     * @return true if worth continuing, false if it would waste computing time
+     */
+    private static boolean isPromising(int k, int i){
+        for(int j=0;j<k;j++){
+            if((posQueens[j]==i) || (Math.abs(posQueens[j]-i) == Math.abs(j-k))){
+                return false;
+            }
+        }
+        //no conflicts found
+        return true;
+    }
+
+    private static void printPositions(){
+        for(int a=0; a<posQueens.length; a++){
+            System.out.print("Row " + a + "'s Queen is on " + posQueens[a] + "\t");
+        }
+        System.out.println();
+    }
 }
